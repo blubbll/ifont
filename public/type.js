4176fea0f3edc95ba916f04da55c0770bd083c6c
Glitch (ifont) <none>
2020-01-02 15:58:19 +0000

    fix .-.

--- public/type.js
+++ public/type.js
@@ -1,44 +1,31 @@
 {
-  //©by Blubbll
-  const $ = window.$;
+    //©by Blubbll
+    const $ = window.$;
 
-  //contruct options gui
-  for (let [k, v] of $.Map) {
-    $("select#font").append(
-      `<option value="${k}">${`${k.charAt(0).toUpperCase()}${k.slice(
+    //contruct options gui
+    for (let [k, v] of $.Map) {
+        $("select#font").append(
+            `<option value="${k}">${`${k.charAt(0).toUpperCase()}${k.slice(
         1
       )}`}</option>`
-    );
-  }
+        );
+    }
 
-  $.update = _ => {
-    let transformed;
-    //requested font
-    const reqFont = $("#font").val();
+    $.update = _ => {
+        let transformed;
+        //requested font
+        const reqFont = $("#font").val();
 
-    //simple transformer :3
-    const transform = o => {
-    
-    o=$.Map.get(reqFont).get(o)
-    
-    Object.keys($.Map.get(reqFont)).forEach((key, i) => {
-      
-       console.log(key)
-      
-      })
-      
-      return o;
-    
-     /* $.Map.get(reqFont).forEach((e, n) => {
-        o = o.replace(new RegExp(n, "g"), e);
-      }),
-      o*/
+        //simple transformer :3
+        const transform = o => {
+            console.log($("input[name=out]").text() + $.Map.get(reqFont).get(o.slice(-1)))
+            $("input[name=out]").val($("input[name=out]").val() + $.Map.get(reqFont).get(o.slice(-1)));
+        };
+
+        //transform
+        transformed = transform($("input[name=in]").val());
+
+        //set transformed result
+        $("input[name=out]").val(transformed);
     };
-
-    //transform
-    transformed = transform($("input[name=in]").val());
-
-    //set transformed result
-    $("input[name=out]").val(transformed);
-  };
 }
